70af3b9df3b85bfb17fd24b2b68011c8eaab2481
Jhonny I. Camargo O. <jhonnycamargo@gmail.com>
2024-09-29 16:28:51 -0400

    Getting class Class from objects.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,21 @@
 package org.example;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException {
+        String hello = "Hello";
+        Class helloClass = hello.getClass();
+
+        Class<?> helloClass2 = "Hello".getClass();
+        Class<?> superClass = helloClass2.getSuperclass();
+        Class<?>[] interfaces = helloClass2.getInterfaces();
+        //Class<String> helloClass3 = "Hello".getClass(); Compile ERROR!!!
+        //Class<Object> helloClass4 = "Hello".getClass(); Compile ERROR!!!
+        Class<? extends String> helloClass5 = "Hello".getClass();
+        Class<? extends Object> helloClass6 = "Hello".getClass();
+        Class<?> stringClass = String.class;
+        String className = "java.lang.String";
+        Class<?> stringClass2 = Class.forName(className);
+
         System.out.println("Hello world!");
     }
 }
