59ebfef30cec237285f6b433d9e2fad28d2a752f
Jhonny I. Camargo O. <jhonnycamargo@gmail.com>
2024-10-19 10:27:33 -0400

    Creating the bean manager to create the entity manager.

--- src/main/java/org/example/ReadingObjects.java
+++ src/main/java/org/example/ReadingObjects.java
@@ -1,10 +1,13 @@
 package org.example;
 
+import org.example.beanmanager.BeanManager;
 import org.example.model.Person;
 import org.example.orm.EntityManager;
 
 public class ReadingObjects {
     public static void main(String[] args) throws Exception {
+        BeanManager beanManager = BeanManager.getInstance();
+        EntityManager<Person> entityManager = beanManager.getInstance(EntityManager.class);
 
         EntityManager<Person> entityManager = EntityManager.of(Person.class);
 
---
+++ src/main/java/org/example/beanmanager/BeanManager.java
@@ -0,0 +1,13 @@
+package org.example.beanmanager;
+
+import org.example.orm.EntityManager;
+
+public class BeanManager {
+    public static BeanManager getInstance() {
+        return null;
+    }
+
+    public <T> EntityManager<T> getInstance(Class<T> clss) {
+        return null;
+    }
+}
