9af3a441b04bc81df8b614d6e45cc106748edbf8
Jhonny I. Camargo O. <jhonnycamargo@gmail.com>
2024-10-19 11:15:21 -0400

    Injecting the Connection value to provide the entity manager.

--- src/main/java/org/example/WritingObjects.java
+++ src/main/java/org/example/WritingObjects.java
@@ -3,6 +3,7 @@ package org.example;
 import org.example.beanmanager.BeanManager;
 import org.example.model.Person;
 import org.example.orm.EntityManager;
+import org.example.orm.ManagedEntityManager;
 
 import java.sql.SQLException;
 import java.util.Arrays;
@@ -10,7 +11,7 @@ import java.util.Arrays;
 public class WritingObjects {
     public static void main(String[] args) throws SQLException, IllegalAccessException {
         BeanManager beanManager = BeanManager.getInstance();
-        EntityManager<Person> entityManager = beanManager.getInstance(EntityManager.class);
+        EntityManager<Person> entityManager = beanManager.getInstance(ManagedEntityManager.class);
 
         Person linda = new Person("Linda", 31);
         Person james = new Person("James", 24);
