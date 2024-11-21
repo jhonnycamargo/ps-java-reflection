5825990f3a96ecb91219a10e25866dc8f67aebcb
Jhonny I. Camargo O. <jhonnycamargo@gmail.com>
2024-11-20 20:30:40 -0500

    Loading default properties from a package.

--- src/main/java/org/example/java8/fundamentals/LoadingPropsFromPackage.java
+++ src/main/java/org/example/java8/fundamentals/LoadingPropsFromPackage.java
@@ -10,21 +10,34 @@ import java.util.Properties;
 public class LoadingPropsFromPackage {
 
     public static void main(String[] args) {
-        try {
+        new LoadingPropsFromPackage().extracted();
+    }
 
+    private void extracted() {
+        try {
+            Properties defaultProps = new Properties();
+            try (InputStream in = getClass().getResourceAsStream("MyDefaultValues.xml")) {
+                defaultProps.loadFromXML(in);
+            }
+            Properties userProps = new Properties(defaultProps);
+            loadUserProps(userProps);
+
+            String displayName = userProps.getProperty("displayName");
+            String appName = userProps.getProperty("app.name");
+
+            System.out.println("displayName: " + displayName);
+            System.out.println("app.name: " + appName);
         } catch (IOException e) {
             System.out.println("Exception <" + e.getClass().getSimpleName() + "> " + e.getMessage());
         }
     }
 
-    private static Properties loadUserProps(Properties userProps) throws IOException {
+    private void loadUserProps(Properties userProps) throws IOException {
         Path userFile = Paths.get("props.xml");
         if (Files.exists(userFile)) {
             try (InputStream in = Files.newInputStream(userFile)) {
                 userProps.loadFromXML(in);
             }
         }
-
-        return userProps;
     }
 }
