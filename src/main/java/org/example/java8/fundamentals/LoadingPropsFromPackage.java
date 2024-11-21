bc3d99e8f828c0f9b1b847e5a2959c97f95997a4
Jhonny I. Camargo O. <jhonnycamargo@gmail.com>
2024-11-20 19:57:22 -0500

    Loading default properties from a package.

--- src/main/java/org/example/java8/fundamentals/LoadingPropsFromPackage.java
+++ src/main/java/org/example/java8/fundamentals/LoadingPropsFromPackage.java
@@ -1,5 +1,30 @@
 package org.example.java8.fundamentals;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Properties;
+
 public class LoadingPropsFromPackage {
 
+    public static void main(String[] args) {
+        try {
+
+        } catch (IOException e) {
+            System.out.println("Exception <" + e.getClass().getSimpleName() + "> " + e.getMessage());
+        }
+    }
+
+    private static Properties loadUserProps(Properties userProps) throws IOException {
+        Path userFile = Paths.get("props.xml");
+        if (Files.exists(userFile)) {
+            try (InputStream in = Files.newInputStream(userFile)) {
+                userProps.loadFromXML(in);
+            }
+        }
+
+        return userProps;
+    }
 }
