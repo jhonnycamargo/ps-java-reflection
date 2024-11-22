bff62f78e698ac8fc18a805460adf1a53da83dbb
Jhonny I. Camargo O. <jhonnycamargo@gmail.com>
2024-11-21 19:27:15 -0500

    Using System properties.

--- src/main/java/org/example/java8/fundamentals/UsingSystemProperties.java
+++ src/main/java/org/example/java8/fundamentals/UsingSystemProperties.java
@@ -3,6 +3,14 @@ package org.example.java8.fundamentals;
 public class UsingSystemProperties {
 
     public static void main(String[] args) {
+        String userName = System.getProperty("user.name");
+        String userHome = System.getProperty("user.home");
+        String osArchitecture = System.getProperty("os.arch");
+        String javaVendor = System.getProperty("java.vendor");
 
+        System.out.println(userName);
+        System.out.println(userHome);
+        System.out.println(osArchitecture);
+        System.out.println(javaVendor);
     }
 }
