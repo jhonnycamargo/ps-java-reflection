96966d883e07d3353989bc3b8bd3ccf50e1c6a25
Jhonny I. Camargo O. <jhonnycamargo@gmail.com>
2024-11-26 18:35:56 -0500

    Parameterized Message Methods.

--- src/main/java/org/example/java8/fundamentals/ParameterizedMessageMethods.java
+++ src/main/java/org/example/java8/fundamentals/ParameterizedMessageMethods.java
@@ -1,14 +1,20 @@
 package org.example.java8.fundamentals;
 
-import java.util.logging.Level;
-import java.util.logging.LogManager;
-import java.util.logging.Logger;
+import java.util.logging.*;
 
 public class ParameterizedMessageMethods {
     private static final Logger logger = LogManager.getLogManager().getLogger(Logger.GLOBAL_LOGGER_NAME);
 
     public static void main(String[] args) {
+
         logger.log(Level.INFO, "{0} is my favorite", "Java");
         logger.log(Level.INFO, "{0} is {1} days from {2}", new Object[]{"Wed", 2, "Fri"});
+
+        doWork("Jim", "Wilson");
+    }
+
+    private static void doWork(String left, String right) {
+        logger.entering("com.jwhh.support.Other", "doWork", new Object[]{left, right});
+
     }
 }
