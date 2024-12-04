06a6f8076780601b8ab1d508b846644d5440359e
Jhonny I. Camargo O. <jhonnycamargo@gmail.com>
2024-12-04 05:54:27 -0500

    OOP stop snoozing feature.

--- src/main/java/org/example/java8/fundamentals/objects/Alarm.java
+++ src/main/java/org/example/java8/fundamentals/objects/Alarm.java
@@ -60,4 +60,12 @@ public class Alarm {
     void sendReport() {
         System.out.println(getReport(true));
     }
+
+    public static void main(String[] args) throws InterruptedException {
+        Alarm alarm = new Alarm("Temperature too high!");
+        alarm.turnOn();
+        alarm.snooze();
+        Thread.sleep(60000*6);
+        alarm.sendReport();
+    }
 }
