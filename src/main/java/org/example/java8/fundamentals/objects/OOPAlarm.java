eaa721f9d6329b36f45b92eddebe11a1f8c57d81
Jhonny I. Camargo O. <jhonnycamargo@gmail.com>
2024-12-07 19:05:57 -0500

    Fields vs properties.

--- src/main/java/org/example/java8/fundamentals/objects/OOPAlarm.java
+++ src/main/java/org/example/java8/fundamentals/objects/OOPAlarm.java
@@ -5,6 +5,6 @@ public class OOPAlarm {
         Alarm alarm = new Alarm("Temperature too high");
         alarm.turnOn();
         alarm.sendReport();
-        alarm.snooze(1);
+        alarm.snooze(20);
     }
 }
