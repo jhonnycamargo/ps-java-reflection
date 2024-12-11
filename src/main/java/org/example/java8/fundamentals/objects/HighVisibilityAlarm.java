a51fbc6e649dbb38e8beb43d08e30b588302e4a4
Jhonny I. Camargo O. <jhonnycamargo@gmail.com>
2024-12-10 19:57:01 -0500

    Cleaning Up the Subclass override annotation.

--- src/main/java/org/example/java8/fundamentals/objects/HighVisibilityAlarm.java
+++ src/main/java/org/example/java8/fundamentals/objects/HighVisibilityAlarm.java
@@ -6,6 +6,7 @@ public class HighVisibilityAlarm extends Alarm {
         super(message);
     }
 
+    @Override
     public String getReport(boolean uppercase) {
         String report = super.getReport(uppercase);
         if (report.isEmpty()) {
