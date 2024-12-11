78ed69372eb28c9a501e1fde822b445140c71c63
Jhonny I. Camargo O. <jhonnycamargo@gmail.com>
2024-12-10 20:15:07 -0500

    Polymorphism.

---
+++ src/main/java/org/example/java8/fundamentals/polymorphism/Program.java
@@ -0,0 +1,9 @@
+package org.example.java8.fundamentals.polymorphism;
+
+import org.example.java8.fundamentals.objects.HighVisibilityAlarm;
+
+public class Program {
+    public static void main(String[] args) {
+        HighVisibilityAlarm alarm = new HighVisibilityAlarm("Pressure low");
+    }
+}
