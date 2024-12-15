04be73f0f28dd0c7e8442dd90e1b289d2c2ccb7a
Jhonny I. Camargo O. <jhonnycamargo@gmail.com>
2024-12-15 09:26:13 -0500

    Upcasting.

--- src/main/java/org/example/java8/fundamentals/polymorphism/Program.java
+++ src/main/java/org/example/java8/fundamentals/polymorphism/Program.java
@@ -1,10 +1,21 @@
 package org.example.java8.fundamentals.polymorphism;
 
+import org.example.java8.fundamentals.objects.Alarm;
 import org.example.java8.fundamentals.objects.HighVisibilityAlarm;
+import org.example.java8.fundamentals.objects.PrioritizedAlarm;
 
 public class Program {
     public static void main(String[] args) {
         HighVisibilityAlarm alarm = new HighVisibilityAlarm("Pressure low");
         alarm.turnOn();
+
+        Alarm alarm2 = new PrioritizedAlarm("Pressure low", 1);
+
+        PrioritizedAlarm myAlarm = new PrioritizedAlarm("Pressure low", 1);
+        showAlarmStatus(myAlarm);
+    }
+
+    private static void showAlarmStatus(Alarm alarm) {
+        System.out.println("Is Snoozing: " + alarm.isSnoozing());
     }
 }
