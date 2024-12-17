f740bb3913127627c97ac95cd50440c026c3ebd6
Jhonny I. Camargo O. <jhonnycamargo@gmail.com>
2024-12-17 18:54:34 -0500

    Intefaces.

--- src/main/java/org/example/java8/fundamentals/objects/TimeSensitiveAlarm.java
+++ src/main/java/org/example/java8/fundamentals/objects/TimeSensitiveAlarm.java
@@ -1,10 +1,16 @@
 package org.example.java8.fundamentals.objects;
 
+import java.awt.*;
 import java.time.LocalDateTime;
 
 public final class TimeSensitiveAlarm extends Alarm {
     public TimeSensitiveAlarm(String message) {
         super(message);
+    }
+
+    @Override
+    public Color getColor() {
+        return Color.BLACK;
     }
 
     @Override
