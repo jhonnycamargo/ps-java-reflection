f3dbab2ab1ac9ec331ecd91d586563a74fd345e6
Jhonny I. Camargo O. <jhonnycamargo@gmail.com>
2024-12-22 10:56:18 -0500

    Using the static keyword.

--- src/main/java/org/example/java8/fundamentals/statickeyword/Alarm.java
+++ src/main/java/org/example/java8/fundamentals/statickeyword/Alarm.java
@@ -3,6 +3,10 @@ package org.example.java8.fundamentals.statickeyword;
 public class Alarm {
     protected boolean active;
 
+    public static String getDocumentation() {
+        return "Use turnOn() to activate.";
+    }
+
     public void turnOn() {
         active = true;
     }
