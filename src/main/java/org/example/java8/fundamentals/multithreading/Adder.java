7218b0893cfea4bd927ebc7c0cd82c828ae6fb79
Jhonny I. Camargo O. <jhonnycamargo@gmail.com>
2024-12-24 17:02:26 -0500

    Multithreading.

--- src/main/java/org/example/java8/fundamentals/multithreading/Adder.java
+++ src/main/java/org/example/java8/fundamentals/multithreading/Adder.java
@@ -2,7 +2,7 @@ package org.example.java8.fundamentals.multithreading;
 
 import java.io.*;
 
-public class Adder {
+public class Adder implements Runnable{
     private String inFile, outFile;
 
     public Adder(String inFile, String outFile) {
@@ -11,20 +11,17 @@ public class Adder {
     }
 
     public void doAdd() throws IOException {
-        int total = 0;
-        String line = null;
 
-        try (BufferedReader reader = new BufferedReader(new FileReader(inFile))) {
-            while ((line = reader.readLine()) != null) {
-                total += Integer.parseInt(line);
-            }
+    }
+
+    @Override
+    public void run() {
+        try {
+            doAdd();
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
 
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(outFile))) {
-            writer.write("Total: " + total);
-        } catch (IOException e) {
-            e.printStackTrace();
 
 }
