32a367b43b1472b4b05d023ddb23caa8d8018727
Jhonny I. Camargo O. <jhonnycamargo@gmail.com>
2024-12-24 17:00:34 -0500

    Multithreading.

--- src/main/java/org/example/java8/fundamentals/multithreading/Adder.java
+++ src/main/java/org/example/java8/fundamentals/multithreading/Adder.java
@@ -1,5 +1,11 @@
 package org.example.java8.fundamentals.multithreading;
 
 public class Adder {
+    private String inFile, outFile;
+
+    public Adder(String inFile, String outFile) {
+        this.inFile = inFile;
+        this.outFile = outFile;
+    }
 
 }
