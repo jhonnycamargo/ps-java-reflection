ae17a40daa235f9879622849a6ebf8dc348823ef
Jhonny I. Camargo O. <jhonnycamargo@gmail.com>
2024-12-25 10:20:05 -0500

    Creating a closer relationship between thread tasks.

--- src/main/java/org/example/java8/fundamentals/multithreading/Adder.java
+++ src/main/java/org/example/java8/fundamentals/multithreading/Adder.java
@@ -1,8 +1,12 @@
 package org.example.java8.fundamentals.multithreading;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
-public class Adder implements Runnable{
+public class Adder implements Runnable {
     private String inFile, outFile;
 
     public Adder(String inFile, String outFile) {
@@ -11,7 +15,16 @@ public class Adder implements Runnable{
     }
 
     public void doAdd() throws IOException {
-
+        int total = 0;
+        String line = null;
+        try (BufferedReader reader = Files.newBufferedReader(Paths.get(inFile))) {
+            while ((line = reader.readLine()) != null) {
+                total += Integer.parseInt(line);
+            }
+        }
+        try (BufferedWriter writer = Files.newBufferedWriter(Paths.get(outFile))) {
+            writer.write("Total: " + total);
+        }
     }
 
     @Override
