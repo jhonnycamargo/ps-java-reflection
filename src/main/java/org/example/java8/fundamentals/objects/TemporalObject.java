65268f1cfa13bf1ad51e6865a4f87160b2bd153b
Jhonny I. Camargo O. <jhonnycamargo@gmail.com>
2025-01-12 17:57:44 -0500

    Creating a closer relationship between thread tasks.

--- src/main/java/org/example/java8/fundamentals/objects/TemporalObject.java
+++ src/main/java/org/example/java8/fundamentals/objects/TemporalObject.java
@@ -2,9 +2,9 @@ package org.example.java8.fundamentals.objects;
 
 public class TemporalObject {
     public static void main(String[] args) {
-        
+
         TemporalObject temporalObject = new TemporalObject();
-        // Call the method to print the current time
+        
         temporalObject.printCurrentTime();
 
     }
