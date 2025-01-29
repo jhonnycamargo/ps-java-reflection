4d05e4f2166e5b8ed483d95c5127668a284af895
Jhonny I. Camargo O. <jhonnycamargo@gmail.com>
2025-01-28 20:27:21 -0500

    Reflection bank account class.

---
+++ src/main/java/org/example/java8/fundamentals/reflection/HighVolumeAccount.java
@@ -0,0 +1,32 @@
+package org.example.java8.fundamentals.reflection;
+
+public class HighVolumeAccount extends BankAccount {
+
+    public HighVolumeAccount(String id) {
+        super(id);
+    }
+
+    public HighVolumeAccount(String id, int balance) {
+        super(id, balance);
+    }
+
+    private int[] readDailyDeposits() {
+        return new int[] {5, 5, 5, 5, 5};
+    }
+
+    private int[] readDailyWithdrawals() {
+        return new int[] {5, 5, 5, 5, 5};
+    }
+
+    public String run() {
+        for (int depositAmt : readDailyDeposits()) {
+            deposit(depositAmt);
+        }
+
+        for (int withdrawalAmt : readDailyWithdrawals()) {
+            withdraw(withdrawalAmt);
+        }
+
+        return "High volume account processed";
+    }
+}
