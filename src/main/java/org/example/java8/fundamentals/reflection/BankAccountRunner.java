c2a50b02a61b469234c8d9728b459f592a02fa0a
Jhonny I. Camargo O. <jhonnycamargo@gmail.com>
2025-01-29 06:13:19 -0500

    Reflection field info.

--- src/main/java/org/example/java8/fundamentals/reflection/BankAccountRunner.java
+++ src/main/java/org/example/java8/fundamentals/reflection/BankAccountRunner.java
@@ -61,18 +61,27 @@ public class BankAccountRunner {
     void fieldInfo(Object obj) {
         Class<?> theClass = obj.getClass();
         Field[] fields = theClass.getFields();
+        displayFields(fields);
 
         Field[] declaredFields = theClass.getDeclaredFields();
     }
 
-
-
-
-
-
-
-
-
+    private void displayFields(Field[] fields) {
+        for (Field field : fields) {
+            System.out.println("Field name: " + field.getName());
+            System.out.println("Field type: " + field.getType().getSimpleName());
+            int modifiers = field.getModifiers();
+            System.out.println("Field modifiers: " + modifiers);
+            System.out.println("Field is public: " + Modifier.isPublic(modifiers));
+            System.out.println("Field is protected: " + Modifier.isProtected(modifiers));
+            System.out.println("Field is private: " + Modifier.isPrivate(modifiers));
+            System.out.println("Field is static: " + Modifier.isStatic(modifiers));
+            System.out.println("Field is final: " + Modifier.isFinal(modifiers));
+            System.out.println("Field is transient: " + Modifier.isTransient(modifiers));
+            System.out.println("Field is volatile: " + Modifier.isVolatile(modifiers));
+            System.out.println();
+        }
+    }
 
 
 }
