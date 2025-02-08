9706415dbdcc69f7bb92768232c9a591c63d8ae1
Jhonny I. Camargo O. <jhonnycamargo@gmail.com>
2025-02-08 18:35:17 -0500

    Add Task worker interface implementation.

--- src/main/java/org/example/java8/fundamentals/reflection/DispatchInvocation.java
+++ src/main/java/org/example/java8/fundamentals/reflection/DispatchInvocation.java
@@ -3,8 +3,19 @@ package org.example.java8.fundamentals.reflection;
 public class DispatchInvocation {
 
     public static void main(String[] args) {
-        BankAccount ba = new BankAccount("1234");
-        AccountWorker aw = new AccountWorker(ba);
-        aw.doWork();
+        BankAccount acct1 = new BankAccount("1234");
+        var dispatchInvocation = new DispatchInvocation();
+        dispatchInvocation.startWork("org.example.java8.fundamentals.reflection.AccountWorker", acct1);
+    }
+
+    void startWork(String workerTypeName, Object workerTarget) {
+        try {
+            Class<?> workerType = Class.forName(workerTypeName);
+            TaskWorker worker = (TaskWorker) workerType.newInstance();
+            worker.setTarget(workerTarget);
+            worker.doWork();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
