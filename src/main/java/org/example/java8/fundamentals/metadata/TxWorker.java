04cf812568d3abf877cfe1026eea80937c456e46
Jhonny I. Camargo O. <jhonnycamargo@gmail.com>
2025-02-11 20:21:24 -0500

    Add metadata.

--- src/main/java/org/example/java8/fundamentals/metadata/TxWorker.java
+++ src/main/java/org/example/java8/fundamentals/metadata/TxWorker.java
@@ -1,4 +1,23 @@
 package org.example.java8.fundamentals.metadata;
 
+import org.example.java8.fundamentals.reflection.BankAccount;
+
 public class TxWorker {
+    protected BankAccount account;
+    protected char txType;
+    protected int amt;
+
+    public TxWorker(BankAccount account, char txType, int amt) {
+        this.account = account;
+        this.txType = txType;
+        this.amt = amt;
+    }
+
+    public void run() {
+        if (txType == 'w') {
+            account.withdraw(amt);
+        } else {
+            account.deposit(amt);
+        }
+    }
 }
