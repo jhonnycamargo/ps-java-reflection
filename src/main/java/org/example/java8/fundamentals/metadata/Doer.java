3ad2704b2f002bbc62b614697c8da05c17f72833
Jhonny I. Camargo O. <jhonnycamargo@gmail.com>
2025-02-12 20:00:54 -0500

    Add metadata.

--- src/main/java/org/example/java8/fundamentals/metadata/Doer.java
+++ src/main/java/org/example/java8/fundamentals/metadata/Doer.java
@@ -2,7 +2,12 @@ package org.example.java8.fundamentals.metadata;
 
 public class Doer {
 
+    @Deprecated
     void doItThisWay() {
         System.out.println("Doing it this way");
+    }
+
+    void doItThisNewWay() {
+        System.out.println("Doing it that way");
     }
 }
