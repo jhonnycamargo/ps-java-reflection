baba8f27576e26370aea3fba227e8717c093a3e0
Jhonny I. Camargo O. <jhonnycamargo@gmail.com>
2025-02-14 18:43:20 -0500

    Add metadata.

--- src/main/java/org/example/java8/fundamentals/metadata/WorkHandler.java
+++ src/main/java/org/example/java8/fundamentals/metadata/WorkHandler.java
@@ -1,7 +1,9 @@
 package org.example.java8.fundamentals.metadata;
 
 import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 
-@Retention()
+@Retention(RetentionPolicy.RUNTIME)
 public @interface WorkHandler {
+    boolean useThreadPool() default false;
 }
