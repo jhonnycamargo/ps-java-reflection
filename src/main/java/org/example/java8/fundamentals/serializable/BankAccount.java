490477f96063c83e41b03899a36e49409f91cd2c
Jhonny I. Camargo O. <jhonnycamargo@gmail.com>
2025-02-19 20:50:37 -0500

    Add serializable.

--- src/main/java/org/example/java8/fundamentals/serializable/BankAccount.java
+++ src/main/java/org/example/java8/fundamentals/serializable/BankAccount.java
@@ -16,6 +16,16 @@ public class BankAccount {
 
     public static void main(String[] args) {
         BankAccount acct1 = new BankAccount("1234", 500);
+        acct1.deposit(100);
         System.out.println(acct1);
+        saveAccount(acct1, "account.dat");
+    }
+
+    private static void saveAccount(BankAccount ba, String fileName) {
+
+    }
+
+    private void deposit(int i) {
+        balance += i;
     }
 }
